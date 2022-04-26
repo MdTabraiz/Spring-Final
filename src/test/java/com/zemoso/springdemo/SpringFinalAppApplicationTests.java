6a4163bdc590bf9dc100102a966e4c875726a38e
MdTabraiz <tabraiz.md@zemosolabs.com>
2022-04-26 10:24:54 +0530

    fix

--- src/test/java/com/zemoso/springdemo/SpringFinalAppApplicationTests.java
+++ src/test/java/com/zemoso/springdemo/SpringFinalAppApplicationTests.java
@@ -1,20 +1,15 @@
 package com.zemoso.springdemo;
 
-import com.zemoso.springdemo.controller.BlogAdminController;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringFinalAppApplicationTests {
 
-	@Autowired
-	BlogAdminController blogAdminController;
-
 	@Test
 	void contextLoads() {
-		Assertions.assertThat(blogAdminController).isNotNull();
+
 	}
 
 }
