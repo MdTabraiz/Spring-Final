47036508c2f01c65fb9cb276c95e1ae4ce48ef5e
MdTabraiz <tabraiz.md@zemosolabs.com>
2022-04-26 10:13:56 +0530

    test added

---
+++ src/test/java/com/zemoso/springdemo/dao/BlogRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.zemoso.springdemo.dao;
+
+import com.zemoso.springdemo.entity.Blog;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+class BlogRepositoryTest {
+
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @BeforeEach
+    public void setUp(){
+        Blog blog = new Blog();
+        blog.setId(1);
+        blog.setTitle("Test Title");
+        blog.setAuthorName("Test Author");
+        blog.setContent("Test Content");
+
+        blogRepository.save(blog);
+    }
+
+
+    @Test
+    void findBlogsByAuthorName() {
+
+        List<Blog> listOfBlogs = blogRepository.findByAuthorName("Test Author");
+
+        Assertions.assertThat(listOfBlogs).isNotEmpty();
+    }
+
+    @Test
+    void findBlogsByFalseAuthorName() {
+
+        List<Blog> listOfBlogs = blogRepository.findByAuthorName("Test False Author");
+
+        Assertions.assertThat(listOfBlogs).isEmpty();
+    }
+}
