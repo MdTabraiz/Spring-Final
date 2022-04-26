1eeffb71cb78f0335ad596aa4bde19cd63882c09
MdTabraiz <tabraiz.md@zemosolabs.com>
2022-04-26 14:57:47 +0530

    test

---
+++ src/test/java/com/zemoso/springdemo/service/BlogServiceTest.java
@@ -0,0 +1,49 @@
+package com.zemoso.springdemo.service;
+
+import com.zemoso.springdemo.dao.BlogRepository;
+import com.zemoso.springdemo.entity.Blog;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class BlogServiceTest {
+
+    @InjectMocks
+    private BlogServiceImpl blogService;
+
+    @Mock
+    private BlogRepository blogRepository;
+
+
+    void setUp() throws Exception{
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindById(){
+        Blog blog = new Blog();
+
+        blog.setId(1);
+        blog.setTitle("Test Title");
+        blog.setAuthorName("Test Author");
+        blog.setContent("Test Content");
+
+        Mockito.when(blogRepository.findById(1)).
+                thenReturn(Optional.of(blog));
+
+        Blog userBlog = blogService.findById(1);
+        Assertions.assertEquals("Test Title",userBlog.getTitle());
+        Assertions.assertEquals("Test Author",userBlog.getAuthorName());
+        Assertions.assertEquals("Test Content",userBlog.getContent());
+
+    }
+
+
+}
